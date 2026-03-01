a1ac4c8f15554959dc0f8c91007ae48de3c96e56
sanjayps24 <sanjayps2407@gmail.com>
2026-03-01 21:59:14 +0530

    Updated the file

--- Amazon_DB.py
+++ Amazon_DB.py
@@ -16,11 +16,11 @@
 #payment modes
 #share a product link
 #upload image and find similar products
- from sqlalchemy import create_engine
+from sqlalchemy import create_engine
 
 Base = declarative_base()
 
- class Cart(Base):
+class Cart(Base):
         __tablename__ = 'cart'
         id = Column(Integer, primary_key=True)
         user_id = Column(Integer)
@@ -32,7 +32,7 @@ Base = declarative_base()
         created_at = Column(DateTime, default=datetime.now())
         updated_at = Column(DateTime, default=datetime.now(), onupdate=datetime.now())
  
- class Wishlist(Base):
+class Wishlist(Base):
         __tablename__ = 'wishlist'
         id = Column(Integer, primary_key=True)
         user_id = Column(Integer)
@@ -41,7 +41,7 @@ Base = declarative_base()
         created_at = Column(DateTime, default=datetime.now())
         updated_at = Column(DateTime, default=datetime.now(), onupdate=datetime.now())
 
- class Product(Base):
+class Product(Base):
         __tablename__ = 'product'
         id = Column(Integer, primary_key=True)
         name = Column(String)
@@ -58,7 +58,7 @@ Base = declarative_base()
         isprime = Column(Boolean)
         taxpercent = Column(Integer)
 
- class ProductReview(Base):
+class ProductReview(Base):
         __tablename__ = 'product_review'
         id = Column(Integer, primary_key=True)
         user_id = Column(Integer, ForeignKey('user.id'))
@@ -68,7 +68,7 @@ Base = declarative_base()
         created_at = Column(DateTime, default=datetime.now())
         updated_at = Column(DateTime, default=datetime.now(), onupdate=datetime.now())
 
- class User(Base):
+class User(Base):
         __tablename__ = 'user'
         id = Column(Integer, primary_key=True)
         name = Column(String)
@@ -79,7 +79,7 @@ Base = declarative_base()
         alt_phone = Column(String)
         isprime = Column(Boolean)
 
-    class Order(Base):
+class Order(Base):
         __tablename__ = 'order'
         id = Column(Integer, primary_key=True)
         user_id = Column(Integer)
@@ -93,7 +93,7 @@ Base = declarative_base()
         payment_type = Column(String)
         payment_status = Column(String)
 
-    class OrderDetails(Base):
+class OrderDetails(Base):
         __tablename__ = 'order_details'
         id = Column(Integer, primary_key=True)
         order_id = Column(Integer, ForeignKey('order.id'))
